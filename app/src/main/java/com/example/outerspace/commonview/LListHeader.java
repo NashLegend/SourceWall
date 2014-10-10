3e1ec3f3cd4265cc5df4c4bda36989155ff0faf5
NashLegend <panzhihuipzh@gmail.com>
2014-10-10 22:52:03 +0800

    detailed

--- app/src/main/java/com/example/outerspace/commonview/LListHeader.java
+++ app/src/main/java/com/example/outerspace/commonview/LListHeader.java
@@ -1,124 +1,196 @@
 package com.example.outerspace.commonview;
 
+import com.example.outerspace.R;
 import com.example.outerspace.commonview.LListView.OnRefreshListener;
 
 import android.content.Context;
-import android.util.AttributeSet;
-import android.view.ViewGroup.MarginLayoutParams;
+import android.view.LayoutInflater;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class LListHeader extends FrameLayout {
+    private int currentState = LListView.State_Normal;
+    private int lastState = currentState;
+    private MarginLayoutParams layoutParams;
+    private OnRefreshListener onRefreshListener;
+    private TextView tvHint;
 
-	private int currentState = LListView.State_Normal;
-	private int lastState = currentState;
-	private MarginLayoutParams layoutParams;
-	private OnRefreshListener onRefreshListener;
+    public LListHeader(Context context) {
+        super(context);
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.layout_header_view, this);
+        tvHint = (TextView) findViewById(R.id.text_header_hint);
+    }
 
-	public LListHeader(Context context) {
-		super(context);
-		// TODO Auto-generated constructor stub
-	}
+    private void checkStateChange() {
+        if (lastState != currentState) {
+            // 在move状态下只有这两种可能
+            if (currentState == LListView.State_Pull_To_Refresh) {
+                //有可能来自下拉从LListView.State_Normal变来也有可能来自上滑从State_Release_To_Refresh变来
+            } else if (currentState == LListView.State_Release_To_Refresh) {
+                //只有可能从State_Pull_To_Refresh变来
+            }
+        }
+    }
 
-	public LListHeader(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		// TODO Auto-generated constructor stub
-	}
+    public boolean handleMoveDistance(float dist) {
+        if (dist < 0 && !isVisible()) {
+            return false;
+        }
 
-	public LListHeader(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		// TODO Auto-generated constructor stub
-	}
+        handleMotion(dist);
 
-	public void switch2Refreshing() {
-		// TODO
-	}
+        if (currentState != LListView.State_Refreshing) {
+            // 这时只有两种可能的状态State_Release_To_Refresh和State_Pull_To_Refresh
+            // TODO 或许可以做更多
+            if (isOverReleaseThreshold()) {
+                currentState = LListView.State_Release_To_Refresh;
+            } else {
+                currentState = LListView.State_Pull_To_Refresh;
+            }
+        }
 
-	public void switch2PullToRefresh() {
-		// TODO
-	}
+        //这段表示状态之间的转换，不涉及状态内动作
+        if (lastState != currentState) {
+            // 在move状态下只有这两种可能
+            if (currentState == LListView.State_Pull_To_Refresh) {
+                //有可能来自下拉从LListView.State_Normal变来也有可能来自上滑从State_Release_To_Refresh变来
+                if (lastState == LListView.State_Normal) {
+                    normal2Pull();
+                } else if (lastState == LListView.State_Release_To_Refresh) {
+                    release2Pull();
+                }
+            } else if (currentState == LListView.State_Release_To_Refresh) {
+                //只有可能从State_Pull_To_Refresh变来
+                pull2Release();
+            }
+        }
+        lastState = currentState;
+        return true;
+    }
 
-	public void switch2ReleaseToRefresh() {
-		// TODO
-	}
+    public void handleOperationDone() {
+        layoutParams = (MarginLayoutParams) getLayoutParams();
+        if (currentState == LListView.State_Release_To_Refresh) {
+            // TODO start refresh
+            if (onRefreshListener != null) {
+                release2Refreshing();
+            } else {
+                release2Normal();
+            }
+        } else if (currentState == LListView.State_Pull_To_Refresh) {
+            pull2Normal();
+        } else if (currentState == LListView.State_Refreshing) {
+            // TODO 这里的值应该是动画的正常高度，是在初始化时就确定的
+            if (getHeight() > 300) {
+                refreshing2Refreshing();
+            }
+        }
+        checkStateChange();
+    }
 
-	private void checkStateChange() {
-		if (lastState != currentState) {
-			// 或许不应该这样，或许应该把motion状态算进来，明天再说……
-		}
-	}
+    private void normal2Refreshing() {
+        tvHint.setText(R.string.refreshing);
+        onRefreshListener.onRefresh();
+    }
 
-	public boolean handleMoveDistance(float dist) {
-		layoutParams = (MarginLayoutParams) getLayoutParams();
-		if (dist < 0 && !isVisible()) {
-			return false;
-		}
-		layoutParams.topMargin += layoutParams.topMargin + dist;
-		setLayoutParams(layoutParams);
-		if (currentState != LListView.State_Refreshing) {
-			// 这时只有两种可能的状态State_Release_To_Refresh和State_Pull_To_Refresh
-			// TODO 或许可以做更多
-			if (layoutParams.topMargin > 0) {
-				currentState = LListView.State_Release_To_Refresh;
-			} else {
-				currentState = LListView.State_Pull_To_Refresh;
-			}
-		}
-		checkStateChange();
-		return true;
-	}
+    private void normal2Pull() {
+        tvHint.setText(R.string.pull_to_refresh);
+    }
 
-	public void handleOperationDone() {
-		layoutParams = (MarginLayoutParams) getLayoutParams();
-		if (currentState == LListView.State_Release_To_Refresh) {
-			// TODO start refresh
-			if (onRefreshListener != null) {
-				animateToRefreshing();
-				onRefreshListener.onRefresh();
-			} else {
-				animateToNormal();
-			}
-		} else if (currentState == LListView.State_Pull_To_Refresh) {
-			animateToNormal();
-		} else if (currentState == LListView.State_Refreshing) {
-			if (layoutParams.topMargin >= 0) {
-				animateToRefreshing();
-			}
-		}
-		checkStateChange();
-	}
+    private void pull2Normal() {
+        tvHint.setText(R.string.idling);
+    }
 
-	private void animateToRefreshing() {
-		// TODO Auto-generated method stub
+    private void pull2Release() {
+        tvHint.setText(R.string.release_to_refresh);
+    }
 
-	}
+    private void release2Pull() {
+        tvHint.setText(R.string.pull_to_refresh);
+    }
 
-	private void animateToNormal() {
-		// TODO Auto-generated method stub
+    private void release2Normal() {
+        tvHint.setText(R.string.idling);
+    }
 
-	}
+    private void release2Refreshing() {
+        tvHint.setText(R.string.refreshing);
+        onRefreshListener.onRefresh();
+    }
 
-	public void handleMotion() {
+    private void refreshing2Refreshing() {
+        //TODO
+    }
 
-	}
+    private void refreshing2Normal() {
+        //
+    }
 
-	@Override
-	protected void onLayout(boolean changed, int left, int top, int right,
-			int bottom) {
-		// TODO Auto-generated method stub
-		super.onLayout(changed, left, top, right, bottom);
-	}
+    /**
+     * 处理运动过程中的变化
+     * 好了问题来了：挖掘机技术到底哪家强？
+     * 不对打错了，假如说我想让header的高度是变化的，那么如果使用margin来控制的话无疑增加了麻烦的计算。
+     * 所以呢最好的方式是不用margin，而是使用直接改变高度的方式
+     *
+     * @param dist
+     */
+    public void handleMotion(float dist) {
+        switch (currentState) {
+            //TODO
+            case LListView.State_Pull_To_Refresh:
 
-	public boolean isVisible() {
-		layoutParams = (MarginLayoutParams) getLayoutParams();
-		return layoutParams.topMargin <= getHeight();
-	}
+                break;
+            case LListView.State_Release_To_Refresh:
 
-	public int getState() {
-		return currentState;
-	}
+                break;
+            case LListView.State_Refreshing:
 
-	public void setOnRefreshListener(OnRefreshListener onRefreshListener) {
-		this.onRefreshListener = onRefreshListener;
-	}
+                break;
+        }
+        getHeight();
+        layoutParams.height += getHeight() + dist;
+        setLayoutParams(layoutParams);
+    }
+
+    public void cancelRefresh() {
+        refreshing2Normal();
+    }
+
+    public void doneRefreshing() {
+        refreshing2Normal();
+    }
+
+    /**
+     * 直接开始刷新，当然前提是当前状态是State_Normal，状态的检测由LListView负责
+     */
+    public void directlyStartRefresh() {
+        normal2Refreshing();
+    }
+
+    private boolean isOverReleaseThreshold() {
+        //这里需要在初始化的时候就确定这个Threshold，TODO
+        return getHeight() > 300;
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right,
+                            int bottom) {
+        // TODO Auto-generated method stub
+        super.onLayout(changed, left, top, right, bottom);
+        layoutParams = (MarginLayoutParams) getLayoutParams();
+    }
+
+    public boolean isVisible() {
+        return getHeight() > 0;
+    }
+
+    public int getState() {
+        return currentState;
+    }
+
+    public void setOnRefreshListener(OnRefreshListener onRefreshListener) {
+        this.onRefreshListener = onRefreshListener;
+    }
 
 }
--- app/src/main/java/com/example/outerspace/commonview/LListView.java
+++ app/src/main/java/com/example/outerspace/commonview/LListView.java
@@ -14,123 +14,160 @@ import android.widget.AbsListView.OnScrollListener;
  */
 public class LListView extends ListView implements OnScrollListener {
 
-	public static final int State_Normal = 0;
-	public static final int State_Pull_To_Refresh = 1;
-	public static final int State_Release_To_Refresh = 2;
-	public static final int State_Refreshing = 3;
-	public static final int State_Loading_More = 4;
+    public static final int State_Normal = 0;
+    public static final int State_Pull_To_Refresh = 1;
+    public static final int State_Release_To_Refresh = 2;
+    public static final int State_Refreshing = 3;
+    public static final int State_Loading_More = 4;
 
-	private LListHeader headerView;
-	private LListFooter footerView;
-	int touchSlop = 16;// The last thing
+    private LListHeader headerView;
+    private LListFooter footerView;
+    int touchSlop = 16;// The last thing
 
-	int headerHeight;
+    private int loadingMoreThreshold = 10;//低于此自动不显示
 
-	private boolean refreshable = false;
+    public int getLoadingMoreThreshold() {
+        return loadingMoreThreshold;
+    }
 
-	public LListView(Context context) {
-		super(context);
-	}
+    public void setLoadingMoreThreshold(int loadingMoreThreshold) {
+        this.loadingMoreThreshold = loadingMoreThreshold;
+    }
 
-	public LListView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		headerHeight = headerView.getHeight();
-	}
 
-	public LListView(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-	}
+    private boolean refreshable = false;
 
-	float touchDownY;
-	float lastY;
-	float currentY;
+    public LListView(Context context) {
+        super(context);
+        addViews(context);
+    }
 
-	@SuppressLint("ClickableViewAccessibility")
-	@Override
-	public boolean onTouchEvent(MotionEvent ev) {
-		if (checkRefreshable(ev)) {
+    public LListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        addViews(context);
+    }
 
-			switch (ev.getAction()) {
-			case MotionEvent.ACTION_DOWN:
-				touchDownY = ev.getY();
-				currentY = touchDownY;
-				lastY = currentY;
-				break;
-			case MotionEvent.ACTION_MOVE:
-				currentY = ev.getY();
-				float dist = currentY - lastY;
-				if (!headerView.handleMoveDistance(dist)) {
-					return super.onTouchEvent(ev);
-				}
-				lastY = currentY;
-				break;
-			case MotionEvent.ACTION_CANCEL:
-			case MotionEvent.ACTION_UP:
-				headerView.handleOperationDone();
-				break;
+    public LListView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        addViews(context);
+    }
 
-			default:
-				break;
-			}
-			setPressed(false);
-			setFocusable(false);
-			setFocusableInTouchMode(false);
-			// TODO 如果是return super.onTouchEvent呢
-			return true;
-		}
-		return super.onTouchEvent(ev);
-	}
+    public void cancelRefresh() {
+        headerView.cancelRefresh();
+    }
 
-	private int getState() {
-		return headerView.getState();
-	}
+    public void doneRefreshing() {
+        headerView.doneRefreshing();
+    }
 
-	private boolean checkRefreshable(MotionEvent ev) {
-		View firstView = this.getChildAt(0);
-		if (firstView != null) {
-			int p = this.getFirstVisiblePosition();
-			if (p == 0 && firstView.getTop() >= 0) {
-				// TODO getTop有么有可能大于0，大于0的时候怎么办。
-				refreshable = true;
-			} else {
-				refreshable = false;
-			}
-		} else {
-			refreshable = true;
-		}
-		return refreshable;
-	}
+    public void startRefresh() {
+        if (getState() == State_Loading_More) {
+            //TODO
+        }
+        if (getState() == State_Normal) {
+            headerView.directlyStartRefresh();
+        }
+    }
 
-	private OnScrollListener outerListener;
+    public void doneLoadingMore() {
 
-	@Override
-	public void setOnScrollListener(OnScrollListener l) {
-		outerListener = l;
-	}
+    }
 
-	@Override
-	public void onScrollStateChanged(AbsListView view, int scrollState) {
-		if (outerListener != null) {
-			outerListener.onScrollStateChanged(view, scrollState);
-		}
-	}
+    private void addViews(Context context) {
+        headerView = new LListHeader(context);
+        addHeaderView(headerView);
+    }
 
-	@Override
-	public void onScroll(AbsListView view, int firstVisibleItem,
-			int visibleItemCount, int totalItemCount) {
-		if (outerListener != null) {
-			outerListener.onScroll(view, firstVisibleItem, visibleItemCount,
-					totalItemCount);
-		}
-	}
+    float touchDownY;
+    float lastY;
+    float currentY;
 
-	public static interface OnRefreshListener {
-		public void onRefresh();
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (checkRefreshable(ev)) {
 
-		public void onLoadMore();
-	}
+            switch (ev.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    touchDownY = ev.getY();
+                    currentY = touchDownY;
+                    lastY = currentY;
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    currentY = ev.getY();
+                    float dist = currentY - lastY;
+                    if (!headerView.handleMoveDistance(dist)) {
+                        return super.onTouchEvent(ev);
+                    }
+                    lastY = currentY;
+                    break;
+                case MotionEvent.ACTION_CANCEL:
+                case MotionEvent.ACTION_UP:
+                    headerView.handleOperationDone();
+                    break;
 
-	public void setOnRefreshListener(OnRefreshListener onRefreshListener) {
-		headerView.setOnRefreshListener(onRefreshListener);
-	}
+                default:
+                    break;
+            }
+            setPressed(false);
+            setFocusable(false);
+            setFocusableInTouchMode(false);
+            // TODO 如果是return super.onTouchEvent呢
+            return true;
+        }
+        return super.onTouchEvent(ev);
+    }
+
+    private int getState() {
+        return headerView.getState();
+    }
+
+    private boolean checkRefreshable(MotionEvent ev) {
+        View firstView = this.getChildAt(0);
+        if (firstView != null) {
+            int p = this.getFirstVisiblePosition();
+            if (p == 0 && firstView.getTop() >= 0) {
+                // TODO getTop有么有可能大于0，大于0的时候怎么办。
+                refreshable = true;
+            } else {
+                refreshable = false;
+            }
+        } else {
+            refreshable = true;
+        }
+        return refreshable;
+    }
+
+    private OnScrollListener outerListener;
+
+    @Override
+    public void setOnScrollListener(OnScrollListener l) {
+        outerListener = l;
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+        if (outerListener != null) {
+            outerListener.onScrollStateChanged(view, scrollState);
+        }
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem,
+                         int visibleItemCount, int totalItemCount) {
+        if (outerListener != null) {
+            outerListener.onScroll(view, firstVisibleItem, visibleItemCount,
+                    totalItemCount);
+        }
+    }
+
+    public static interface OnRefreshListener {
+        public void onRefresh();
+
+        public void onLoadMore();
+    }
+
+    public void setOnRefreshListener(OnRefreshListener onRefreshListener) {
+        headerView.setOnRefreshListener(onRefreshListener);
+    }
 }
