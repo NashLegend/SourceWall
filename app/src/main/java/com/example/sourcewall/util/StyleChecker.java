de9b60dfe5e25e5bc4e085b937d56502cb31a2c0
NashLegend <panzhihuipzh@gmail.com>
2014-12-09 20:27:43 +0800

    只是一个TODO，网站的样式有可能会变化，但是又不能直接使用它的css，要使用改过的才成

---
+++ app/src/main/java/com/example/sourcewall/util/StyleChecker.java
@@ -0,0 +1,9 @@
+package com.example.sourcewall.util;
+
+/**
+ * Created by NashLegend on 2014/12/9 0009
+ * 果壳的样式变来变去，要不要加一个检查样式变化的类，发现变化后重新请求
+ * 这是一个 TODO 的需要，最后再说……
+ */
+public class StyleChecker {
+}
