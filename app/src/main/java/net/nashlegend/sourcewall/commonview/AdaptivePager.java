5e42c56f170f9eb1dd87778ec1e97cf01edb72f1
Peter <pzh@xiaoge.co>
2015-10-16 11:07:41 +0800

    pager

---
+++ app/src/main/java/net/nashlegend/sourcewall/commonview/AdaptivePager.java
@@ -0,0 +1,39 @@
+package net.nashlegend.sourcewall.commonview;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by NashLegend on 2015/10/16 0016.
+ */
+public class AdaptivePager extends ViewPager {
+    public AdaptivePager(Context context) {
+        super(context);
+    }
+
+    public AdaptivePager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        try {
+            return super.onTouchEvent(ev);
+        } catch (IllegalArgumentException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (IllegalArgumentException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+}
