6a5c29598c57315b12e5f91552f1bae6e2fe3863
NashLegend <panzhihuipzh@gmail.com>
2016-07-26 23:27:37 +0800

    style

--- app/src/main/java/net/nashlegend/sourcewall/fragment/ProfileFragment.java
+++ app/src/main/java/net/nashlegend/sourcewall/fragment/ProfileFragment.java
@@ -11,12 +11,16 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.umeng.analytics.MobclickAgent;
 
 import net.nashlegend.sourcewall.R;
 import net.nashlegend.sourcewall.activities.LoginActivity;
+import net.nashlegend.sourcewall.activities.SettingActivity;
 import net.nashlegend.sourcewall.request.api.UserAPI;
 import net.nashlegend.sourcewall.util.Consts;
 import net.nashlegend.sourcewall.util.ImageUtils;
+import net.nashlegend.sourcewall.util.Mob;
+import net.nashlegend.sourcewall.util.SharedPreferencesUtil;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -31,6 +35,22 @@ public class ProfileFragment extends BaseFragment {
     TextView userName;
     @BindView(R.id.profile_header)
     LinearLayout profileHeader;
+    @BindView(R.id.layout_msg_center)
+    LinearLayout layoutMsgCenter;
+    @BindView(R.id.layout_my_favors)
+    LinearLayout layoutMyFavors;
+    @BindView(R.id.layout_my_posts)
+    LinearLayout layoutMyPosts;
+    @BindView(R.id.layout_my_replies)
+    LinearLayout layoutMyReplies;
+    @BindView(R.id.layout_my_questions)
+    LinearLayout layoutMyQuestions;
+    @BindView(R.id.view_switch_to_day)
+    LinearLayout viewSwitchToDay;
+    @BindView(R.id.view_switch_to_night)
+    LinearLayout viewSwitchToNight;
+    @BindView(R.id.layout_setting)
+    LinearLayout layoutSetting;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -69,17 +89,51 @@ public class ProfileFragment extends BaseFragment {
             imageAvatar.setImageResource(R.drawable.ic_default_avatar_96dp);
             userName.setText(R.string.click_to_login);
         }
+        if (SharedPreferencesUtil.readBoolean(Consts.Key_Is_Night_Mode, false)) {
+            viewSwitchToDay.setVisibility(View.VISIBLE);
+            viewSwitchToNight.setVisibility(View.GONE);
+        } else {
+            viewSwitchToDay.setVisibility(View.GONE);
+            viewSwitchToNight.setVisibility(View.VISIBLE);
+        }
     }
 
-    @OnClick(R.id.profile_header)
-    public void onClick(View v) {
-        switch (v.getId()) {
+    @OnClick({R.id.layout_msg_center, R.id.layout_my_favors, R.id.layout_my_posts,
+            R.id.layout_my_replies, R.id.layout_my_questions, R.id.view_switch_to_day,
+            R.id.view_switch_to_night, R.id.layout_setting, R.id.profile_header})
+    public void onClick(View view) {
+        switch (view.getId()) {
             case R.id.profile_header:
                 if (!UserAPI.isLoggedIn()) {
                     Intent intent = new Intent(getActivity(), LoginActivity.class);
                     startActivityForResult(intent, Consts.Code_Login);
                 }
                 break;
+            case R.id.layout_msg_center:
+                break;
+            case R.id.layout_my_favors:
+                break;
+            case R.id.layout_my_posts:
+                break;
+            case R.id.layout_my_replies:
+                break;
+            case R.id.layout_my_questions:
+                break;
+            case R.id.view_switch_to_day:
+            case R.id.view_switch_to_night:
+                revertMode();
+                break;
+            case R.id.layout_setting:
+                startActivity(new Intent(getActivity(), SettingActivity.class));
+                break;
         }
     }
+
+
+    private void revertMode() {
+        SharedPreferencesUtil.saveBoolean(Consts.Key_Is_Night_Mode, !SharedPreferencesUtil.readBoolean(Consts.Key_Is_Night_Mode, false));
+        MobclickAgent.onEvent(getActivity(), Mob.Event_Switch_Day_Night_Mode);
+        getActivity().recreate();
+    }
+
 }
