d9e56684ae904811f39f9e3284e575435d976763
dev@xiaoge.co <dev@xiaoge.co>
2016-08-26 16:19:06 +0800

    update db

--- app/src/main/java/net/nashlegend/sourcewall/db/gen/DaoMaster.java
+++ app/src/main/java/net/nashlegend/sourcewall/db/gen/DaoMaster.java
@@ -58,9 +58,8 @@ public class DaoMaster extends AbstractDaoMaster {
 
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            Log.i("greenDAO", "Upgrading schema from version " + oldVersion + " to " + newVersion + " by dropping all tables");
-            dropAllTables(db, true);
-            onCreate(db);
+//            dropAllTables(db, true);
+            createAllTables(db, true);
         }
     }
 
